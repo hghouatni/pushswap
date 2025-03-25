1c4c1c90fe6faf6ef8e9fef700ad74310809e0cb
Hicham Ghoutani <hghoutan@c2r9p10.1337.ma>
2025-03-25 13:33:27 +0000

    save state 2

--- pushswap.c
+++ pushswap.c
@@ -6,7 +6,7 @@
 /*   By: hghoutan <hghoutan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 16:30:21 by macbook           #+#    #+#             */
-/*   Updated: 2025/03/24 16:45:51 by hghoutan         ###   ########.fr       */
+/*   Updated: 2025/03/25 13:30:38 by hghoutan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int ft_exist(Stack *stack, int num) {
     return 0;
 }
 
-void free_split(char **split_args) {
+void free_all(char **split_args) {
     int i;
     
     i = 0;
@@ -33,6 +33,34 @@ void free_split(char **split_args) {
         i++;
     }
     free(split_args);
+}
+
+long	ft_atoi2(const char *str)
+{
+	int	ng;
+	long	num;
+
+	ng = 1;
+	num = 0;
+
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			ng = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		num = (num * 10) + (*str - '0');
+        if (num > 2147483647 && ng == 1)
+            return (2147483648);
+        if (num > 2147483648 && ng == -1)
+            return (-2147483649);
+		str++;
+	}
+	return (num * ng);
 }
 
 int validate_input(char **argv, Stack *stack) {
@@ -48,24 +76,24 @@ int validate_input(char **argv, Stack *stack) {
         k = 0;
         while (split_args[k]) {
             j = 0;
-            if (split_args[k][j] == '-')
+            if (split_args[k][j] == '-' || split_args[k][j] == '+')
                 j++;
             if (split_args[k][j] == '\0')
-                return 0;
+                return (free_all(split_args), 0);
             while (split_args[k][j]) {
                 if ((split_args[k][j] < '0' || split_args[k][j] > '9'))
-                    return 0;
+                    return (free_all(split_args), 0);
                 j++;
             }
-            num = ft_atoi(split_args[k]);
+            num = ft_atoi2(split_args[k]);
             if (num > 2147483647 || num < -2147483648)
-                return 0;
+                return (free_all(split_args), 0);
             if (ft_exist(stack, num))
-                return 0;
+                return (free_all(split_args), 0);
             push_stack(stack, num);
             k++;
         }
-        free_split(split_args);
+        free_all(split_args);
         i++;
     }
     return 1;
@@ -87,13 +115,11 @@ void push_stack(Stack *stack, int data) {
     Node *current;
     
     node = createNode(data);
-    if (stack->head == NULL) {
-        stack->head = node;
-        return;
-    }
-    current = stack->head;
     if (node == NULL)
         return;
+    if (stack->head == NULL) 
+        return (void)(stack->head = node);
+    current = stack->head;
     while (current->next)
         current = current->next;
     current->next = node;
@@ -104,14 +130,83 @@ void print_stack(Node *head) {
 
   current = head;
   while (current) {
-    printf("%ld\n", (long)current->data);
+    printf("%d\n", current->data);
     current = current->next;
   }
+}
+
+int is_sorted(Stack *stack) {
+    Node *current;
+    int max;
+
+    current = stack->head;
+    while (current->next) {
+        if (current->data > (current->next->data))
+            return (0);
+        current = current->next;
+    }
+    return (1);
+}
+
+int get_size(Stack *stack) {
+    Node *current;
+    int size;
+    
+    size = 0;
+    current = stack->head;
+    while (current) {
+        size++;
+        current = current->next;
+    }
+    return (size);
+}
+
+void sort_small_stack(Stack *stack_a, Stack *stack_b, int size) {
+    if (size == 2) {
+        sa(stack_a);
+    }
+    // else if (size == 3) {
+    //     if (stack_a->head->data > stack_a->head->next->data && stack_a->head->data < stack_a->head->next->next->data)
+    //         sa(stack_a);
+    //     else if (stack_a->head->data > stack_a->head->next->data && stack_a->head->data > stack_a->head->next->next->data)
+    //     {
+    //         sa(stack_a);
+    //         rra(stack_a);
+    //     }
+    //     else if (stack_a->head->data < stack_a->head->next->data && stack_a->head->data > stack_a->head->next->next->data)
+    //         ra(stack_a);
+    //     else if (stack_a->head->data < stack_a->head->next->data && stack_a->head->data < stack_a->head->next->next->data)
+    //     {
+    //         sa(stack_a);
+    //         ra(stack_a);
+    //     }
+    // }
+    // else if (size == 4) {
+    //     pb(stack_a, stack_b);
+    //     pb(stack_a, stack_b);
+    //     sort_small_stack(stack_a, stack_b, 2);
+    //     pa(stack_b, stack_a);
+    //     pa(stack_b, stack_a);
+    // }
+}
+
+void free_stack(Stack *stack) {
+    Node *current;
+    Node *temp;
+
+    current = stack->head;
+    while (current) {
+        temp = current;
+        current = current->next;
+        free(temp);
+    }
+    stack->head = NULL;
 }
 
 int main(int argc, char **argv)
 {
     int i;
+    int size;
     Stack stack_a;
     Stack stack_b;
     
@@ -122,33 +217,41 @@ int main(int argc, char **argv)
     stack_b.head = NULL;
     if (validate_input(argv, &stack_a))
     {
-        //sa(&stack_a);
-        // pb(&stack_a, &stack_b);
-        // pb(&stack_a, &stack_b);
-        // pb(&stack_a, &stack_b);
+        // if (is_sorted(&stack_a))
+        //     return (0);
+        // size = get_size(&stack_a);
+        // if (size < 5)
+        //     sort_small_stack(&stack_a, &stack_b, size);
         
-         ra(&stack_a);
-        // rb(&stack_b);
+        // //sa(&stack_a);
+        // // pb(&stack_a, &stack_b);
+        // // pb(&stack_a, &stack_b);
+        // // pb(&stack_a, &stack_b);
+        
+        //  ra(&stack_a);
+        // // rb(&stack_b);
 
-        //rr(&stack_a, &stack_b);
+        // //rr(&stack_a, &stack_b);
         
-        // rra(&stack_a);
-        // rrb(&stack_b);
+        // // rra(&stack_a);
+        // // rrb(&stack_b);
         
-        // sb(&stack_b);
-        // rra(&stack_a);
-        // pb(&stack_a, &stack_b);
-        // pb(&stack_a, &stack_b);
+        // // sb(&stack_b);
+        // // rra(&stack_a);
+        // // pb(&stack_a, &stack_b);
+        // // pb(&stack_a, &stack_b);
         
         
-        print_stack((&stack_a)->head);
-        printf("\n");
-        printf("\n");
-        print_stack((&stack_b)->head);
+        // print_stack((&stack_a)->head);
+        // printf("\n");
+        // printf("\n");
+        // print_stack((&stack_b)->head);
     
     }
     else
         write(1, "Error\n", 6);
+    free_stack(&stack_a);
+    free_stack(&stack_b);
     return (0);
 }
 
